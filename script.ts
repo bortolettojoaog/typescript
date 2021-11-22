a7cbd0bc95b90b7392dd32699600ce0f3bc45a28
João G. Bortoletto <jgb3532@hotmail.com>
2021-11-22 18:42:44 -0300

    :package: v1.0

--- script.ts
+++ script.ts
@@ -8,5 +8,12 @@ function sumTwoNumbers(e) {
     var n1 = parseInt((<HTMLInputElement>document.getElementById("number1")).value);
     var n2 = parseInt((<HTMLInputElement>document.getElementById("number2")).value);
 
+    addElement();
+
     alert(sum(n1, n2));
+}
+
+function addElement() {
+    const element: HTMLElement = document.getElementById('main') as HTMLElement;
+    element.innerHTML = `<div class="warn"><p style="text-align: center;">Sum performed successfully!</p> <a style="display: block; text-decoration: none; text-align: center;" href="index.html">Home</a></div>`;
 }
